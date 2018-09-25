5e387c61f327f0d8ec983d759c33ea9c27be1c15
Thomas Noe <thomas.noe@madwiremedia.com>
2018-09-24 20:09:04 -0600

    change Hash to be specific; comment

--- hash/hash.go
+++ hash/hash.go
@@ -5,13 +5,16 @@ import (
 	"encoding/base64"
 )
 
+// HashService provides an interface for string hashing
 type HashService interface {
 	Calculate(str string) string
 }
 
-type Hash struct{}
+// HashSha512 implements SHA512 hashing
+type HashSha512 struct{}
 
-func (h *Hash) Calculate(str string) string {
+// Calculate returns a SHA512 sum for a given string
+func (h *HashSha512) Calculate(str string) string {
 	sha := sha512.Sum512([]byte(str))
 	return base64.StdEncoding.EncodeToString(sha[:])
 }
--- hash/hash_test.go
+++ hash/hash_test.go
@@ -8,7 +8,7 @@ func TestCalculate(t *testing.T) {
 	seedText := "angryMonkey"
 	expected := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
 
-	hash := &Hash{}
+	hash := &HashSha512{}
 
 	result := hash.Calculate(seedText)
 
