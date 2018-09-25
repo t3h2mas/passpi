8035783eddc0821161b33d49c890ad5b9bd1c7ff
Thomas Noe <thomas.noe@madwiremedia.com>
2018-09-24 19:30:30 -0600

    initial test case

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -3,5 +3,14 @@ package hash
 import "testing"
 
 func TestCalculate(t *testing.T) {
-	t.Error("Not implemented")
+	seedText := "angryMonkey"
+	expected := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZP ZklJz0Fd7su2A+gf7Q=="
+
+	hash := &Hash{}
+
+	result, _ := hash.Calculate(seedText)
+
+	if result != expected {
+		t.Errorf("expected '%s', got '%s'", expected, result)
+	}
 }
