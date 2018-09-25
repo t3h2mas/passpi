151cff0e53508f3112184d1e6c823723710e77ed
Thomas Noe <thomas.noe@madwiremedia.com>
2018-09-24 20:15:16 -0600

    table tests

--- hash/hash_test.go
+++ hash/hash_test.go
@@ -5,14 +5,27 @@ import (
 )
 
 func TestCalculate(t *testing.T) {
-	seedText := "angryMonkey"
-	expected := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
+	var tests = []struct {
+		inp      string
+		expected string
+	}{
+		{
+			"angryMonkey",
+			"ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q==",
+		},
+		{
+			"",
+			"z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg==",
+		},
+	}
 
 	hash := &HashSha512{}
 
-	result := hash.Calculate(seedText)
+	for _, tt := range tests {
+		result := hash.Calculate(tt.inp)
+		if result != tt.expected {
+			t.Errorf("expected '%s', got '%s'", tt.expected, result)
+		}
 
-	if result != expected {
-		t.Errorf("expected '%s', got '%s'", expected, result)
 	}
 }
