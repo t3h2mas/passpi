adf237bf36d43e9cee2f15ea26679f208e5c9df0
Thomas Noe <thomas.noe@madwiremedia.com>
2018-09-26 19:27:46 -0600

    update logging

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -15,44 +16,47 @@ type server struct {
 	router *http.ServeMux
 }
 
+func httpErr(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
 func (s *server) handleHash() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			fmt.Fprintf(w, "Not allowed")
+			httpErr(w, http.StatusMethodNotAllowed)
 			return
 		}
 
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Bad request")
+			log.Printf("Failed to parse body, error: %s\n", err.Error())
+			httpErr(w, http.StatusBadRequest)
 			return
 		}
 
 		// body must not be empty
 		if len(body) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Bad request")
+			log.Println("Body empty")
+			httpErr(w, http.StatusBadRequest)
 			return
 		}
 
 		pieces := strings.Split(string(body), "=")
 		if len(pieces) != 2 {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Bad request")
+			log.Printf("Bad body format:\n%s\n", string(body))
+			httpErr(w, http.StatusBadRequest)
 			return
 		}
 
 		if pieces[0] != "password" {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Bad request")
+			log.Printf("Bad body format:\n%s\n", string(body))
+			httpErr(w, http.StatusBadRequest)
 			return
 		}
 
 		if len(pieces[1]) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Bad request")
+			log.Printf("Password field empty:\n%s\n", string(body))
+			httpErr(w, http.StatusBadRequest)
 			return
 		}
 
@@ -68,4 +72,12 @@ func (s *server) delayMiddleware(duration time.Duration) func(http.HandlerFunc) 
 			next.ServeHTTP(w, r)
 		}
 	}
+}
+
+// decorates router
+func (s *server) logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
