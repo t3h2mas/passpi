8d9440b1a4e2ac55a119b95da88f0e3d7b0a71a1
Thomas Noe <thomas.noe@madwiremedia.com>
2018-10-01 20:40:15 -0600

    remove quick hash route

--- routes.go
+++ routes.go
@@ -4,7 +4,6 @@ import "time"
 
 // register router handlers
 func (s *server) routes(delay time.Duration) {
-	s.router.HandleFunc("/hashQuick", s.handleHash())
 	s.router.HandleFunc("/hash", s.statsMiddleware(s.delayMiddleware(delay)(s.handleHash())))
 	s.router.HandleFunc("/shutdown", s.handleShutdown())
 	s.router.HandleFunc("/stats", s.handleStats())
